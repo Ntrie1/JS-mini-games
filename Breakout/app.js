2eeadd278968e10a7598935eb97713dfa8907951
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-03-13 15:59:49 +0200

    created user block

--- Breakout/app.js
+++ Breakout/app.js
@@ -1,7 +1,10 @@
 const grid = document.querySelector('.grid');
 
-const blockWidth = 100
-const blockHeight = 20
+const blockWidth = 100;
+const blockHeight = 20;
+
+const userStart = [230, 10];
+let currentPosition = userStart;
 
 class Block {
     constructor(xAxis, yAxis) {
@@ -23,13 +26,12 @@ const blocks = [
     new Block(230, 240),
     new Block(340, 240),
     new Block(450, 240),
-    new Block(10, 270),
-    new Block(10, 270),
-    new Block(10, 270),
-    new Block(10, 270),
-    new Block(10, 270),
-    new Block(10, 270),
-   
+    new Block(10, 210),
+    new Block(120, 210),
+    new Block(230, 210),
+    new Block(340, 210),
+    new Block(450, 210),
+
 ];
 
 console.log(blocks[0]);
@@ -39,10 +41,18 @@ function addBlocks() {
         const block = document.createElement('div');
         block.classList.add('block');
         block.style.left = b.bottomLeft[0] + 'px'
-        block.style.bottom = b.bottomLeft[1] +'px'
+        block.style.bottom = b.bottomLeft[1] + 'px'
         grid.appendChild(block);
     })
 
 }
 
 addBlocks();
+
+
+const user = document.createElement('div');
+user.classList.add('user');
+user.style.left = currentPosition[0] + 'px';
+user.style.bottom = currentPosition[1] + 'px';
+grid.appendChild(user);
+
