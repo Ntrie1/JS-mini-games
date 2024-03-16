8a46f7a4ed7db08fbcbcb9af30670f9c48d03a6b
Ntrie1 <114010893+Ntrie1@users.noreply.github.com>
2024-03-16 18:32:41 +0200

    logic for moving left logs

--- Frogger/app.js
+++ Frogger/app.js
@@ -2,6 +2,7 @@ const timeLeftDisplay = document.getElementById('time-left');
 const resultDisplay = document.getElementById('result');
 const startPauseButton = document.getElementById('start-pause-button');
 const squares = document.querySelectorAll('.grid div');
+const logsLeft = document.querySelectorAll('.log-left');
 
 let currentIndex = 76
 const width = 9;
@@ -24,8 +25,38 @@ function moveFrog(e) {
     }
 
     squares[currentIndex].classList.add('frog');
-
-
-
 }
+
 document.addEventListener('keyup', moveFrog);
+
+function autoMoveLogs() {
+    logsLeft.forEach((log) => moveLogsLeft(log))
+}
+
+
+function moveLogsLeft(logLeft) {
+    switch (true) {
+        case logLeft.classList.contains('l1'):
+            logLeft.classList.remove('l1');
+            logLeft.classList.add('l2');
+            break;
+        case logLeft.classList.contains('l2'):
+            logLeft.classList.remove('l2');
+            logLeft.classList.add('l3');
+            break;
+        case logLeft.classList.contains('l3'):
+            logLeft.classList.remove('l3');
+            logLeft.classList.add('l4');
+            break;
+        case logLeft.classList.contains('l4'):
+            logLeft.classList.remove('l4');
+            logLeft.classList.add('l5');
+            break;
+        case logLeft.classList.contains('l5'):
+            logLeft.classList.remove('l5');
+            logLeft.classList.add('l1');
+            break;
+    }
+}
+
+setInterval(autoMoveLogs, 1000);
